5958308e7062a38529605e4cb318e98e06d776a1
buildautomation <buildautomation@syncfusion.com>
2026-07-07 05:18:29 +0000

    v34.1.29

---
+++ showcase/spreadsheet/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using syncfusion.spreadsheetdemo.wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Syncfusion.SfSkinManager;
+
+namespace Spreadsheet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SfSkinManager.ApplyThemeAsDefaultStyle = true;
+            var window = Activator.CreateInstance(typeof(SpreadSheetDemo)) as Window;
+            window.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ showcase/spreadsheet/Views/SpreadSheetDemo.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace syncfusion.spreadsheetdemo.wpf
+{
+    /// <summary>
+    /// Interaction logic for SpreadSheetDemo.xaml
+    /// </summary>
+    public partial class SpreadSheetDemo : RibbonWindow
+    {
+        public SpreadSheetDemo()
+        {
+            InitializeComponent();
+            this.Unloaded += SpreadSheetDemo_Unloaded;
+        }
+
+        private void SpreadSheetDemo_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (spreadsheetRibbon != null)
+            {
+                spreadsheetRibbon.Dispose();
+                spreadsheetRibbon = null;
+            }
+
+            if (spreadSheetControl != null)
+            {
+                spreadSheetControl.Dispose();
+                spreadSheetControl = null;
+            }
+
+            this.Unloaded -= SpreadSheetDemo_Unloaded;
+        }
+    }
+}
