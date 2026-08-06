8641eb354206f35586c351d1b8e9701ece4fb99c
buildautomation <buildautomation@syncfusion.com>
2026-08-06 07:01:57 +0000

    v34.2.2

--- README.md
+++ README.md
@@ -59,4 +59,4 @@ Syncfusion has no liability for any damage or consequence that may arise by the 
 * To renew the subscription, click [here](https://www.syncfusion.com/sales/products) or contact our sales team at <salessupport@syncfusion.com>.
 
 * Don't see what you need? Please request it in our [Feedback portal](https://www.syncfusion.com/feedback/file-formats).
-<p>Copyright © 2001-2026 Syncfusion, Inc. Updated on 2026-07-07 at precisely 05:18:27 EST.</p> 
+<p>Copyright © 2001-2026 Syncfusion, Inc. Updated on 2026-08-06 at precisely 07:01:56 EST.</p> 
---
+++ showcase/spreadsheet/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using syncfusion.spreadsheetdemo.wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Syncfusion.SfSkinManager;
+
+namespace Spreadsheet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Syncfusion.Telemetry.Telemetry.Disable();
+            SfSkinManager.ApplyThemeAsDefaultStyle = true;
+            var window = Activator.CreateInstance(typeof(SpreadSheetDemo)) as Window;
+            window.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
